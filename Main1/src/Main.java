4897d2010c41f13257852692c95c5a339fe6b48c
satsuki <satsuki@DESKTOP-RKSUEQT>
2026-01-14 18:58:45 +0900

    1/14分

---
+++ Main1/src/Main.java
@@ -0,0 +1,12 @@
+//インクリメント演算子
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		a = 100;
+		a++; // aの値が1増える　a = a + 1と同じ
+		System.out.println(a);
+	}
+
+}
