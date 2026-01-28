0ba3a35ea87693e9925e54f9214a6664a59b8a25
satsuki <satsuki@DESKTOP-RKSUEQT>
2026-01-28 16:34:29 +0900

    1/28 step8

--- trainingspring4/bk/springboot_bk/step6/UserService.java
+++ trainingspring4/bk/springboot_bk/step6/UserService.java
@@ -1,13 +1,10 @@
 package com.example.demo.service;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.example.demo.dto.UserUpdateRequest;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
@@ -15,7 +12,6 @@ import com.example.demo.repository.UserRepository;
  * ユーザー情報 Service
  */
 @Service
-@Transactional(rollbackFor = Exception.class)
 public class UserService {
 	/**
 	 * ユーザー情報 Repository
@@ -37,18 +33,5 @@ public class UserService {
 	 */
 	public User findById(Long id) {
 		return userRepository.findById(id).get();
-	}
-
-	/**
-	 * ユーザー情報 更新
-	 * @param user ユーザー情報
-	 */
-	public void update(UserUpdateRequest userUpdateRequest) {
-		User user = findById(userUpdateRequest.getId());
-		user.setAddress(userUpdateRequest.getAddress());
-		user.setName(userUpdateRequest.getName());
-		user.setPhone(userUpdateRequest.getPhone());
-		user.setUpdateDate(new Date());
-		userRepository.save(user);
 	}
 }
