c13ef7666fca3032fb0bc012f5fdd1bbabb330e8
satsuki <satsuki@DESKTOP-RKSUEQT>
2026-01-29 19:02:52 +0900

    a

---
+++ moattravel/src/main/resources/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS house (
+	
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	
+	name VARCHAR(50) NOT NULL,
+	
+	image_name VARCHAR(255),
+	
+	description VARCHAR(255) NOT NULL,
+	
+	price INT NOT NULL,
+	
+	capacity INT NOT NULL,
+	
+	postal_cade VARCHAR(50) NOT NULL,
+	
+	address VARCHAR(255) NOT NULL,
+	
+	
+	phone_number VARCHAR(50) NOT NULL,
+	
+	created_at DATETIME NOT NULL DEFAUT	CURRENT_TIMESTAMP
+	
+upeddete_at DATETIME NOT NULL DEFAULT
+	
+	
+	
+	
+	
+	
+)
