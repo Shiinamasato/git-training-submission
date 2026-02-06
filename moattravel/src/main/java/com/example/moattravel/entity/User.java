5d783ea65f031c12667a8bbf873ef9c2955d5b10
satsuki <satsuki@DESKTOP-RKSUEQT>
2026-02-06 14:30:53 +0900

    2/6 14:00

--- moattravel/src/main/java/com/example/moattravel/entity/User.java
+++ moattravel/src/main/java/com/example/moattravel/entity/User.java
@@ -34,6 +34,9 @@ public class User {
 
 	@Column(name = "address")
 	private String address;
+	
+	@Column(name = "phone_number")
+	private String phoneNumber;
 
 	@Column(name = "email")
 	private String email;
